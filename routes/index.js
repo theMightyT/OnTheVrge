e408a72c303d8b7f371a178d67d40f69a8ec26e0
theMightyT <tvanrys@fanshaweonline.ca>
2019-03-16 12:36:34 -0400

    updates...

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Welcome to OnTheVrge', message: 'Get ready to experience poetry! The best poetry ever!' });
 });
 
 module.exports = router;
